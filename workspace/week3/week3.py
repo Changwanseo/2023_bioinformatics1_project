4e636dd1d1081e7bf7fde912f9c1945459f9d32c
Changwan Seo <wan101010@snu.ac.kr>
2023-05-17 12:57:24 +0900

    3.1.0
    - Week3 submission

--- workspace/week3/week3.py
+++ workspace/week3/week3.py
@@ -1,17 +1,56 @@
 import pandas as pd
 import re
+import math
 
-pileup = pd.read_csv(
-    "CLIP-let7g-gene.pileup",
-    sep="\t",
-    names=["chrom", "pos", "_ref", "count", "basereads", "quals"],
-)
-pileup.tail()
 
-toremove = re.compile("[<>$*#^]")
-pileup["matches"] = pileup["basereads"].apply(lambda x: toremove.sub("", x))
+def write_bedgraph(gene, position):
+    pileup = pd.read_csv(
+        f"CLIP-{gene}-gene.pileup",
+        sep="\t",
+        names=["chrom", "pos", "_ref", "count", "basereads", "quals"],
+    )
+    pileup.tail()
 
-print(pileup[["chrom", "pos", "matches"]])
-print(pileup[pileup["pos"] == 106056094].iloc[0]["matches"])
+    toremove = re.compile("[<>$*#^]")
+    pileup["matches"] = pileup["basereads"].apply(lambda x: toremove.sub("", x))
 
-print(pileup)
+    # print(pileup[["chrom", "pos", "matches"]])
+    # print(pileup[pileup["pos"] == 106056094].iloc[0]["matches"])
+
+    # Assisted with chatgpt
+    def calculate_shannon_entropy(string):
+        base_counts = {}
+        total_bases = 0
+
+        for base in string:
+            if base in base_counts:
+                base_counts[base] += 1
+            else:
+                base_counts[base] = 1
+            total_bases += 1
+
+        entropy = 0.0
+        for count in base_counts.values():
+            frequency = count / total_bases
+            entropy -= frequency * math.log2(frequency)
+        return entropy
+
+    pileup["shannon"] = pileup["matches"].apply(calculate_shannon_entropy)
+
+    pileup["end"] = pileup["pos"] + 1
+
+    bedgraph = pileup[["chrom", "pos", "end", "shannon"]]
+    bedgraph.to_csv("tmp_csv", sep="\t", header=False, index=False)
+
+    header = f'browser position {position}\nbrowser hide all\nbrowser pack refGene encodeRegions\nbrowser full altGraph\ntrack type=bedGraph name="{gene}" description="{gene}" visibility=full color=200,100,0 altColor=0,100,200 priority=20\n'
+
+    with open("tmp_csv", "r") as f:
+        content = f.read()
+        with open(f"{gene}.bedgraph", "a") as fw:
+            fw.write(header)
+            fw.write(content)
+
+
+write_bedgraph(gene="let7g", position="chr9:106056039-106056126")
+write_bedgraph(gene="let7f-1", position="chr13:48691305-48691393")
+write_bedgraph(gene="let7d", position="chr13:48689488-48689590")
--- workspace/week3/week3.sh
+++ workspace/week3/week3.sh
@@ -10,22 +10,22 @@ tail CLIP-let7g-gene.pileup
 
 #chr13:48691305-48691393
 grep -i mirlet7f-1 ../../data/gencode.vM27.annotation.gtf
-samtools view -b -o CLIP-let7f-1.bam ../../data/project/CLIP-35L33G.bam chr9:106056039-106056126
+samtools view -b -o CLIP-let7f-1.bam ../../data/project/CLIP-35L33G.bam chr13:48691305-48691393
 samtools view CLIP-let7f-1.bam | wc -l
 samtools mpileup CLIP-let7f-1.bam > CLIP-let7f-1.pileup
 wc -l CLIP-let7f-1.pileup
 head CLIP-let7f-1.pileup
-awk '$2 >= 106056039 && $2 <= 106056126 { print $0; }' CLIP-let7f-1.pileup > CLIP-let7f-1-gene.pileup
+awk '$2 >= 48691305 && $2 <= 48691393 { print $0; }' CLIP-let7f-1.pileup > CLIP-let7f-1-gene.pileup
 tail CLIP-let7f-1-gene.pileup
 
 
 
-#chr13:48689488-8689590
+#chr13:48689488-48689590
 grep -i mirlet7d ../../data/gencode.vM27.annotation.gtf
-samtools view -b -o CLIP-let7d.bam ../../data/project/CLIP-35L33G.bam chr9:106056039-106056126
+samtools view -b -o CLIP-let7d.bam ../../data/project/CLIP-35L33G.bam chr13:48689488-48689590
 samtools view CLIP-let7d.bam | wc -l
 samtools mpileup CLIP-let7d.bam > CLIP-let7d.pileup
 wc -l CLIP-let7d.pileup
 head CLIP-let7d.pileup
-awk '$2 >= 106056039 && $2 <= 106056126 { print $0; }' CLIP-let7d.pileup > CLIP-let7d-gene.pileup
+awk '$2 >= 48689488 && $2 <= 48689590 { print $0; }' CLIP-let7d.pileup > CLIP-let7d-gene.pileup
 tail CLIP-let7d-gene.pileup
